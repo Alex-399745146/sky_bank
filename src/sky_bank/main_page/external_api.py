6b92628502126913edc4369212102281357310a2
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-02-16 00:34:25 +0300

    finish 5 tasks(funcs) for main_page

--- src/sky_bank/main_page/external_api.py
+++ src/sky_bank/main_page/external_api.py
@@ -1,10 +1,11 @@
 """Модуль external_api.py содержит функции работающие c валютами а волатильность обновляет API"""
 
 import os
-# from http.client import responses
-# import pandas as pd
+import apimoex
+import pandas as pd
 import cbrapi
 import requests
+
 from src.sky_bank.data_extract import get_data_json
 from datetime import datetime, timedelta
 
@@ -40,56 +41,33 @@ def get_current_exchange_rate(money_list: list[str]) -> list[dict[str, str | flo
 
     return result
 
+
 # Task_5 Стоимость акций Мосбиржи сегодня.
-def get_current_stock_price(stocks_list):
+def get_current_stock_price(tickers_list: list) -> dict | None:
     """ Актуализируем цены на акции по API(открытый) в Мосбирже """
-    securities = ",".join(stocks_list)
-    url = (
-        f"https://iss.moex.com/iss/engines/stock/markets/shares/securities/"
-        f"{securities}/securities.json"
-    )
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()  # Проверяем HTTP-статус
-    data = response.json()
-    prices = data
-    # # Извлекаем цены из ответа
-    # prices = {}
-    # for ticker in stocks_list:
-    #     # Ищем данные по тикеру в ответе
-    #     found = False
-    #     for row in data["securities"]["data"]:
-    #         if row[0] == ticker:  # row[0] — тикер
-    #             prices[ticker] = float(row[11])  # row[11] — текущая цена (LAST)
-    #             found = True
-    #             break
-    #     if not found:
-    #         prices[ticker] = None  # Если данных нет
+    url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json'
+    params = {
+        'iss.only': 'marketdata',
+        'marketdata.columns': 'SECID,LAST'
+    }
 
-    return prices
+    # Запрос в Мосбиржу на последние цены всех акций.
+    with requests.Session() as session:
+        # Фильтруем столбцы(коды_акций, последние_цены)
+        client = apimoex.ISSClient(session, url, params)
+        data = client.get()
 
-    # currency_codes: str = currency_codes["operationAmount"]["currency"]["code"]
-    # amount: float = currency_codes["operationAmount"]["amount"]
-    #
-    # convert_in = tx_currency_code
-    # convert_out = "RUB"
+    # Проверка наличия данных в ответе
+    if 'marketdata' in data and data['marketdata']:
+        prices_df = pd.DataFrame(data['marketdata'])
+        data = prices_df[prices_df['SECID'].isin(tickers_list)]
+        data = data.set_index('SECID') # убираем столбец порядковых номеров
+        result = data.to_dict()['LAST']
 
-
-    # url = f"https://api.apilayer.com/exchangerates_data/convert?to={convert_out}&from={convert_in}&amount={amount}"
-    # headers = {"apikey": os.getenv("API_KEY")}
-
-    # try:
-    #     response = requests.get(url, headers=headers, timeout=10)
-    #     response.raise_for_status()  # Проверить, произошла ли ошибка во время запроса.
-    #     return response.status_code
-    #
-    # #     if response.status_code == 200:
-    # #         response_api = response.json()
-    # #         print(response_api)
-    # #         return round(response_api["result"], 2)
-    # except requests.exceptions.RequestException as err:
-    #     raise ConnectionError(f"Ошибка запроса к API: {err}")
-    # except (KeyError, ValueError, TypeError) as err:
-    #     raise ValueError(f"Некорректный ответ от API: {err}")
+        return result
+    else:
+        print("Ошибка: данные о ценах не получены (пустой ответ API)")
+        return None
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -101,31 +79,12 @@ if __name__ == "__main__":  # pragma: no cover
     stock_codes: list[str] = config_user['user_stocks']
 
     # Проверка работы: get_current_exchange_rate
-    # data_currency = get_current_exchange_rate(currency_codes)
-    # print(data_currency)
-    # for i in data_currency:
-    #     # print(type(i))
-    #     print(i)
+    data_currency = get_current_exchange_rate(currency_codes)
+    for price in data_currency:
+        print(price)
+    print()
 
     # Проверка работы: get_current_stock_price
     data_stock = get_current_stock_price(stock_codes)
-    # print(type(data_stock))
-    # print(data_stock)
-    # print('---------------------')
     for key, value in data_stock.items():
-        # print(type(i))
-        print(key, value)
-
-
-    # url = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json?interval=1"
-    # response = requests.get(url)
-    # data = response.json()
-    #
-    # print(response.status_code)
-    # print(type(data))
-    # print(data)
-
-
-    # # Парсинг данных
-    # last_price = data['candles']['data'][-1][4](https: // pypi.org / project / apimoex /)  # 4 — индекс цены закрытия
-    # print("Последняя цена SBER:", last_price)
+        print(f'stock: {key} price: {value}')
